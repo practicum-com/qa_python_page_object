683be5d82c2fa1709199aa9084d66d64c7a1bc5c
svetit-sa <115215274+svetit-sa@users.noreply.github.com>
2023-08-02 16:53:31 +0300

    Update page_object_methods1.py

--- page_object_methods1.py
+++ page_object_methods1.py
@@ -2,33 +2,33 @@ from selenium.webdriver.common.by import By
 
 
 class HomePageAround:
-    # The Add button locator1
+    # El localizador del botón Agregar
     add_new_place_button = (By.CLASS_NAME, 'profile__add-button')
-    # The Name field locator
+    # El localizador del campo Nombre
     name_field = (By.NAME, 'name')
-    # The Link-to-the-image field locator
+    # El localizador del campo Enlace a la imagen
     link_to_picture_field = (By.NAME, 'link')
-    # The Save button locator
-    save_button = (By.XPATH, ".//form[@name='new-card']/button[text()='Save']")
+    # El localizador del botón Guardar
+    save_button = (By.XPATH, ".//form[@name='new-card']/button[text()='Guardar']")
 
     def __init__(self, driver):
         self.driver = driver
 
-    # The method clicks on the Add button
+    # El método hace clic en el botón Agregar
     def click_add_new_place_button(self):
         self.driver.find_element(*self.add_new_place_button).click()
 
-    # The method enters the name of the new place
+    # El método introduce el nombre del nuevo lugar
     def set_name(self):
         new_title = "Новое место"
         self.driver.find_element(*self.name_field).send_keys(new_title)
 
-    # The method enters a link to the image
+    # El método introduce un enlace a la imagen
     def set_link_to_picture_field(self):
-        self.driver.find_element(*self.link_to_picture_field).send_keys("Link to the image")
+        self.driver.find_element(*self.link_to_picture_field).send_keys("Enlace a la imagen")
 
-    # The method clicks on the Save button
+    # El método hace clic en el botón Guardar
     def click_save_button(self):
         self.driver.find_element(*self.save_button).click()
 
-    # The step to add a new place
+    # El paso para agregar un nuevo lugar
